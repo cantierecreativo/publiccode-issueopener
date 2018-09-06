273842073b303f345c025dd6092f771ef8fac164
Silvio Relli <silvio@relli.it>
2018-09-06 16:03:44 +0200

    Improved runner script

--- bin/runner.rb
+++ bin/runner.rb
@@ -3,7 +3,15 @@ require "dotenv/load"
 
 while true do
   puts "Running main script"
-  system("ruby", "main.rb")
+  begin
+    system "pwd"
+    load "./bin/main.rb"
+  rescue StandardError => e
+    puts "------------- Error running main script --------------"
+    puts e.message
+    puts e.backtrace.join("\n")
+    puts "------------------------------------------------------"
+  end
   puts "Sleeping #{ENV["EXEC_INTERVAL"]} seconds..."
   sleep ENV["EXEC_INTERVAL"].to_i
 end
